fb0b20254fbc3f6b02e75aaaacd700ce53c9858e
Мария <logacheva-33@mail.ru>
2024-11-11 19:28:24 +0300

    homework1

---
+++ module_2_3_py.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+number = [42, 69, 322, 13, 0, 99, 9, 8, 7, 5]
+
+while i < len(my_list)):
+    my_list = int(input(my_list))
+    print(my_list)
+
+    if number[i] < 0:
+        break
+    print(number[i])
+
+
