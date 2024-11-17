034a780c964c69ea243027a591bb4f85a94d9461
Мария <logacheva-33@mail.ru>
2024-11-17 20:49:48 +0300

    homework1

---
+++ module_2_hard.py
@@ -0,0 +1,8 @@
+my_list = []
+for elem in range(3, 21):
+    text = f' наш элемент: {elem}'
+    for x in range(1, elem):
+        for y in range(1, elem):
+            if elem % (x + y) == 0:
+                text += f"{x}{y}"
+                print(text)
