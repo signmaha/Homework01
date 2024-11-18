6fbc5b162b8be4ccf426f44ef0b8ff96c7595bb6
Мария <logacheva-33@mail.ru>
2024-11-18 21:04:25 +0300

    homework1

--- matrix.py
+++ matrix.py
@@ -1,15 +1,17 @@
 def get_matrix(n, m, value):
-    matrix = []
-    for i in range (n): #внешний цикл для строк
-        mat = []
-    for j in range (m):#внутренний цикл для столбцов
-        mat.append(value)
-    matrix.append(mat)
-    return(matrix)
+    matrix = [] + []
+    for row in range(n):  # внешний цикл для строк
+        matrix.append([])
+    for column in range(m):  # внутренний цикл для столбцов
+        matrix[row].append(value)
+    # matrix.append(row)
+    return (matrix)
 
-result1 = get_matrix(2, 2, 3)
-result2 = get_matrix(3, 6, 15)
-result3 = get_matrix(5, 12, 60)
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 6, 42)
+result3 = get_matrix(5, 2, 15)
+
 print(result1)
 print(result2)
 print(result3)
