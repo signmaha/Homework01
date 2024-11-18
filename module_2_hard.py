be440a89c151c2c3e75a1d276fa4e5cd35e554e1
Мария <logacheva-33@mail.ru>
2024-11-18 21:04:40 +0300

    homework1

--- module_2_hard.py
+++ module_2_hard.py
@@ -4,5 +4,5 @@ for elem in range(3, 21):
     for x in range(1, elem):
         for y in range(1, elem):
             if elem % (x + y) == 0:
-                text += f"{x}{y}"
-                print(text)
+                text += f'{x} + {y} '
+    print(text)
