d0ca7aef7542d03dd12a9d0c15f9ccf944d573e7
Мария <logacheva-33@mail.ru>
2024-11-26 23:27:43 +0300

    homework1

--- module_2_3_py.py
+++ module_2_3_py.py
@@ -1,8 +1,8 @@
 my_list = [42, 69, 322, 13, 0, 99, 9, 8, 7, 5]
 index_lst = 0
 print(len(my_list))
-while my_list[index_lst] > -1:
-    if my_list[index_lst] == -1:
+while my_list[index_lst] > 0:
+    if my_list[index_lst] == 0:
 
         continue
     if (my_list[index_lst]) >= len(my_list):
